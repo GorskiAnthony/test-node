13fb43fb0d4c935e0a5c361ca5600878d052844b
Anthony Gorski <anthony.developpeurweb@gmail.com>
2022-09-29 11:11:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const multiplication = require("./multiplication");
+
+// console.log(multiplication(5, 3));
+// console.log(multiplication(8, 2));
+// console.log(multiplication(16, 12));
+// console.log(multiplication(7, 13));
+// console.log(multiplication(42, 42));
+
+const chalk = require("chalk");
+
+// import chalk from "chalk";
+
+console.log(chalk.bgYellow.black("Hello Les CrewStillants!"));
